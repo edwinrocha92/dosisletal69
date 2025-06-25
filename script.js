94a647a249e07f6eff91515462016e0fe3b59f6c
Edwin Rocha <edwinrocha@MacBook-Pro-de-Edwin.local>
2025-06-25 13:38:45 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Inicializar Parallax de Materialize
+document.addEventListener('DOMContentLoaded', function () {
+    const elems = document.querySelectorAll('.parallax');
+    M.Parallax.init(elems);
+  });
+  
+
+
+  
