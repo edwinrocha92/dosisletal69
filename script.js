7726276ed0670c53c29e69ede8909a089c51ea04
Edwin <edwin.rochaquezada92@gmail.com>
2025-06-25 21:00:13 -0600

    links

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const elems = document.querySelectorAll('.parallax');
     M.Parallax.init(elems);
   });
-  
 
+
+function enviarPedido(producto, precio) {
+  const destinatario = "example@gmail.com";
+  const asunto = encodeURIComponent(`Pedido - ${producto}`);
+
+  const textoBody = `
+    🧢 Producto: ${producto}
+    💲 Precio: ${precio} 
+    
+    Gracias por tu compra.
+    
+    Dosis Letal 69
+    `;
+
+  const mailtoLink = `mailto:${destinatario}?subject=${asunto}&body=${encodeURIComponent(textoBody)}`;
+  window.open(mailtoLink, "_blank");
+}
 
   
